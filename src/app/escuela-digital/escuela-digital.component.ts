30bfde68852b215d74401fb89e1af437b33ef260
Alberto156 <lm5708144@gmail.com>
2021-08-25 00:37:53 -0400

    Base app

---
+++ src/app/escuela-digital/escuela-digital.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ed-escuela-digital',
+  templateUrl: './escuela-digital.component.html',
+  styleUrls: ['./escuela-digital.component.css']
+})
+export class EscuelaDigitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("Componente iniciado")
+  }
+
+}
